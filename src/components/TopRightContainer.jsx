ad11f6c38c20e8de509adcda8432eee89eaf9013
Sebastian Stankiewicz <sebastianstankiewicz@notforyouruse.local>
2024-04-17 21:06:08 +0100

    Add custom domain. Fixed bug again

--- src/components/TopRightContainer.jsx
+++ src/components/TopRightContainer.jsx
@@ -6,11 +6,7 @@ function TopRightContainer() {
   return (
     <div className='max-w-xs max-h-full'>
       <div className='text-cyan-50 flex flex-col items-center'>
-        <img
-          src={runningImage}
-          alt="Photo of me"
-          className="mb-4 max-w-full"
-        />
+        
         <div className='flex'>
           <div className='ml-auto mr-4'><FaGithub /></div>
           <div className='ml-auto'><FaLinkedin /></div>
